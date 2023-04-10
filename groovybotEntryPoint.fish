b83dceb79ff00448dca9532fb3d31685154fd73b
AMathMonkey <amathmonkey@gmail.com>
2023-04-10 01:06:13 -0400

    Don't need to use tee Also add fish version for testing

--- groovybotEntryPoint.fish
+++ groovybotEntryPoint.fish
@@ -1,6 +1,6 @@
-cd "$(dirname -- "$BASH_SOURCE")"
-while true; do
+cd (status --current-filename)
+while true
     coffee groovybot.coffee >> log.txt 2>&1
-    echo "$(date -u): PROCESS ENDED, SLEEPING FOR 1 HOUR"
+    echo (date -u)": PROCESS ENDED, SLEEPING FOR 1 HOUR"
     sleep 1h # sleep and then try again after an hour if it exits 
-done
+end
