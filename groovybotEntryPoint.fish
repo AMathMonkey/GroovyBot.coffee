c3dee99fdc7eca64e3f32a23e3c22b80869693a5
AMathMonkey <amathmonkey@gmail.com>
2023-04-10 01:13:33 -0400

    Removing tee was a mistake, undo. Also bugfix.

--- groovybotEntryPoint.fish
+++ groovybotEntryPoint.fish
@@ -1,6 +1,6 @@
-cd (status --current-filename)
+cd (dirname (status --current-filename))
 while true
-    coffee groovybot.coffee >> log.txt 2>&1
+    coffee groovybot.coffee &| tee -a log.txt
     echo (date -u)": PROCESS ENDED, SLEEPING FOR 1 HOUR"
     sleep 1h # sleep and then try again after an hour if it exits 
 end
--- groovybotEntryPoint.sh
+++ groovybotEntryPoint.sh
@@ -1,6 +1,6 @@
 cd "$(dirname -- "$BASH_SOURCE")"
 while true; do
-    coffee groovybot.coffee >> log.txt 2>&1
+    coffee groovybot.coffee |& tee -a log.txt
     echo "$(date -u): PROCESS ENDED, SLEEPING FOR 1 HOUR"
     sleep 1h # sleep and then try again after an hour if it exits 
 done
