c6e5cdc995ef81e10d14536deba5b3840f858d81
AMathMonkey <amathmonkey@gmail.com>
2024-09-07 02:38:14 -0400

    Run from dir where .env file is present

--- groovybotEntryPoint.fish
+++ groovybotEntryPoint.fish
@@ -1,9 +1,8 @@
 cd (dirname (status --current-filename))
 test -d build; and rm -rf build
 coffee --compile --output build .
-cd build
 while true
-    node groovybot.js &| tee -a log.txt
+    node build/groovybot.js &| tee -a log.txt
     echo (date -u)": PROCESS ENDED, SLEEPING FOR 1 HOUR"
     sleep 1h # sleep and then try again after an hour if it exits 
 end
--- groovybotEntryPoint.sh
+++ groovybotEntryPoint.sh
@@ -1,9 +1,8 @@
 cd "$(dirname -- "$BASH_SOURCE")"
 [[ -d build ]] && rm -rf build
 coffee --compile --output build .
-cd build
 while true; do
-    node groovybot.js |& tee -a log.txt
+    node build/groovybot.js |& tee -a log.txt
     echo "$(date -u): PROCESS ENDED, SLEEPING FOR 1 HOUR"
     sleep 1h # sleep and then try again after an hour if it exits 
 done
